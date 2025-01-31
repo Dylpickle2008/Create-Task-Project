a8e5ede8f4a3b562d844031332273de87ad3b1be
Dylpickle2008 <dylanrigby1016@gmail.com>
2025-01-31 13:32:31 +0000

    started js

---
+++ task.js
@@ -0,0 +1,11 @@
+let submitContainer = document.querySelector('submit-container');
+const leftButton = document.querySelector('.leftbutton'); 
+const rightButton = document.querySelector('.rightbutton');
+const submitButton = document.querySelector('.submit');
+let counter = document.querySelector('#counter');
+const addWord = document.querySelector('.add-word');
+let inputContainer = document.querySelector('.input-container');
+let words = ['volcano', 'aa', 'aal', 'aalii', 'aaliis', 'aals', 'aas', 'acari', 'acarine', 'acarines', 'acarpellous', 'acarpelous', 'acarpous',]
+let currentIndex = -1; 
+
+
