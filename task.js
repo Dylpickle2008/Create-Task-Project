c3da34ad5ade0dfc375d7556046aa9a081cef153
Dylpickle2008 <dylanrigby1016@gmail.com>
2025-02-03 13:32:41 +0000

    added js pt 2

--- task.js
+++ task.js
@@ -4,8 +4,34 @@ const rightButton = document.querySelector('.rightbutton');
 const submitButton = document.querySelector('.submit');
 let counter = document.querySelector('#counter');
 const addWord = document.querySelector('.add-word');
-let inputContainer = document.querySelector('.input-container');
+let mirror = document.querySelector('.mirror');
 let words = ['volcano', 'aa', 'aal', 'aalii', 'aaliis', 'aals', 'aas', 'acari', 'acarine', 'acarines', 'acarpellous', 'acarpelous', 'acarpous',]
 let currentIndex = -1; 
 
 
+document.querySelector('.submit').addEventListener('click', function() {
+    let mirrorText = document.querySelector('.add-word').value;
+    words.push(mirrorText);
+    console.log(words);
+    let newMirror = document.createElement('div');
+    newMirror.className = 'mirror';
+    newMirror.innerHTML = `<h3>${mirrorText}</h3>`;
+    document.querySelector('mirror-input').value = "";
+});
+
+
+leftButton.addEventListener('click', function() { 
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateMirror();
+        updateCounter();
+    }
+});
+
+rightButton.addEventListener('click', function() { 
+    if (currentIndex < reminders.length - 1) {
+        currentIndex++;
+        updateMirror();
+        updateCounter();
+    }
+});
